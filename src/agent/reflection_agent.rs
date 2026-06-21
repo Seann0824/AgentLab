79a3b32c2b332ca438813b374ce7a56670039af0
sean0824 <sean@englife.space>
2026-06-22 02:02:52 +0800

    feat: reflection agent

--- src/agent/reflection_agent.rs
+++ src/agent/reflection_agent.rs
@@ -1,232 +1,211 @@
-struct ReflectionAgent {
-    llm_client: openai::OpenaiChatCompletionClient,
+use openai_api_rs::v1::chat_completion::{FinishReason, chat_completion_stream::ChatCompletionStreamResponse};
+
+use crate::{base::{agent::{Agent, AgentBase, }, config::Config, llm::AgentsLLM, message::Message }, tools::ToolManager};
+use std::io::{self, Write};
+use futures_util::stream::StreamExt;
+
+pub struct ReflectionAgent {
+    base: AgentBase,
     tool_manager: ToolManager,
-    messages: Vec<ChatCompletionMessage>,
-    max_iterations: usize,
+    prompt_templates: Option<ReflectionPromptTemplates>,
+    max_steps: u64,
+}
+
+#[derive(Clone)]
+pub struct ReflectionPromptTemplates {
+    pub initial: String,
+    pub reflect: String,
+    pub refine: String,
+}
+
+impl ReflectionPromptTemplates {
+    fn iter(&self) -> [(&str, &str); 3] {
+        [
+            ("initial", &self.initial),
+            ("reflect", &self.reflect),
+            ("refine", &self.refine),
+        ]
+    }
+}
+
+impl Default for ReflectionPromptTemplates {
+    fn default() -> Self {
+        Self { initial: Default::default(), reflect: Default::default(), refine: Default::default() }
+    }
 }
 
 impl ReflectionAgent {
-    fn new(llm_client: openai::OpenaiChatCompletionClient, tool_manager: ToolManager) -> Self {
+    pub fn new(
+        name: impl Into<String>, 
+        llm: AgentsLLM,
+        prompt_templates: impl Into<Option<ReflectionPromptTemplates>>,
+        config: impl Into<Option<Config>>,
+        tool_manager: impl Into<Option<ToolManager>>,
+        max_steps: impl Into<Option<u64>>,
+    ) -> Self {
+        let config = config.into().unwrap_or(Config::from_env());
+        let tool_manager = tool_manager.into().unwrap_or(ToolManager::new());
+        let max_steps = max_steps.into().unwrap_or(5);
+        let prompt_templates = prompt_templates.into();
+        let agent_base = AgentBase::new(
+            name.into(),
+            llm, 
+            Some("".to_string()), 
+            Some(config),
+        );
+
         Self {
-            llm_client,
-            messages: vec![],
             tool_manager,
-            max_iterations: 3
+            base: agent_base,
+            max_steps,
+            prompt_templates,
         }
     }
 
-    async fn run(&mut self) {
+    pub async fn run_reflection(&mut self, task: &str) -> String {
+        let Some(prompt_templates) = self.prompt_templates.clone() else {
+            return self.run(task).await;
+        };
+
+        let mut last_result: Option<String> = None;
+        let mut final_result = String::new();
+        self.base.clear_history();
+
+        for (_key, template) in prompt_templates.iter() {
+            let mut prompt = template.replace("{task}", task);
+
+            if let Some(content) = &last_result {
+                prompt = prompt.replace("{content}", content);
+            }
+
+            final_result = self.run(&prompt).await;
+            last_result = Some(final_result.clone());
+        }
+
+        final_result
+    }
+}
+
+impl Agent for ReflectionAgent {
+    fn base(&self) -> &AgentBase {
+        &self.base
+    }
+
+    fn base_mut(&mut self) -> &mut AgentBase {
+        &mut self.base
+    }
+
+    async fn run(&mut self, input_text: &str) -> String {
+        println!("🤖 {} 正在处理: {input_text}", self.base.name);
+        io::stdout().flush().ok();
+        let user_message = Message::user(input_text, None);
+        self.add_message(user_message);
+        let mut is_continue = true;
+        let mut current_step = 0u64;
+        let mut final_resut = String::new();
         loop {
-            let mut question = String::new();
-            if self.messages.is_empty() || self.messages.last().is_some_and(|last_message| last_message.role != MessageRole::tool) {
-                println!("\nUser: ");
-                std::io::stdin()
-                    .read_line(&mut question)
-                    .unwrap();
-            }
-            let Some(mut code) = self.execution(&question).await else {
+            if !is_continue {
                 break;
-            };
-            for i in 0..self.max_iterations {
-                let Some(feedback) = self.reflection(&question, &code).await else {
-                    break;
-                };
-                let Some(refinement_code) = self.refinement(&question, &code, &feedback).await else {
-                    break;
-                };
-                code = refinement_code;
             }
-        }
-    }
+            let history_message = self.base
+                .get_history()
+                .into_iter()
+                .map(|message| message.naive_message)
+                .collect();
+            let mut agent_stream = self.base.llm.invoke(
+                history_message, 
+                Some(self.tool_manager.get_tools_scehma()),
+                None
+            ).await;
+            let (mut reason_delta, mut content_delta) = (vec![], vec![]);
+            let mut tools_call = None;
 
-    async fn execution(&mut self, task: &str) -> Option<String> {
-        let prompt = format!(r#"
-            你是一位资深的Python程序员。请根据以下要求，编写一个Python函数。
-            你的代码必须包含完整的函数签名、文档字符串，并遵循PEP 8编码规范。
+            while let Some(chunck) = agent_stream.next().await {
+                match chunck {
+                    ChatCompletionStreamResponse::Content(delta) => {
+                        print!("{}", delta);
+                        content_delta.push(delta);
 
-            要求: {task}
+                    },
+                    ChatCompletionStreamResponse::Reasoning(delta) => {
+                        reason_delta.push(delta);
+                    },
+                    ChatCompletionStreamResponse::ToolCall(tc) => {
+                        tools_call = Some(tc);
+                    },
+                    ChatCompletionStreamResponse::Done(finish_reason)=> {
+                        // 区分调用工具和没有调用工具的信息
+                        // 1. 判断当时是否是工具调用
+                        let Some(finish_reason) = finish_reason else {
+                            panic!("end failed");
+                        };
 
-            请直接输出代码，不要包含任何额外的解释。
-        "#);
-
-        self.messages.push(
-            ChatCompletionMessage {
-                role: MessageRole::user,
-                content: Content::Text(prompt),
-                name: None,
-                tool_call_id: None,
-                tool_calls: None
-            }
-        );
-        let think_stream = self.llm_client.think(self.messages.clone(), None, None).await;
-        let (code, is_no_call_tool) = self.process_think_stream(Box::pin(think_stream), None).await;
-        if is_no_call_tool {
-            Some(code)
-        } else {
-            None
-        }
-    }
-
-    async fn reflection(&mut self, task: &str, code: &str) -> Option<String> {
-        let prompt = format!(r#"
-            你是一位极其严格的代码评审专家和资深算法工程师，对代码的性能有极致的要求。
-            你的任务是审查以下Python代码，并专注于找出其在<strong>算法效率</strong>上的主要瓶颈。
-
-            # 原始任务:
-            {task}
-
-            # 待审查的代码:
-            ```python
-            {code}
-            ```
-
-            请分析该代码的时间复杂度，并思考是否存在一种<strong>算法上更优</strong>的解决方案来显著提升性能。
-            如果存在，请清晰地指出当前算法的不足，并提出具体的、可行的改进算法建议（例如，使用筛法替代试除法）。
-            如果代码在算法层面已经达到最优，才能回答“无需改进”。
-
-            请直接输出你的反馈，不要包含任何额外的解释。
-        "#);
-        let messages = vec![
-            ChatCompletionMessage {
-                role: MessageRole::user,
-                content: Content::Text(prompt),
-                name: None,
-                tool_call_id: None,
-                tool_calls: None
-            }
-        ];
-        let think_stream = self.llm_client.think(messages, None, None).await;
-        let (feedback, is_no_call_tool) = self.process_think_stream(Box::pin(think_stream), Some(false)).await;
-        if is_no_call_tool && !feedback.eq("无需改进") {
-            Some(feedback)
-        } else {
-            None
-        }
-    }
-
-    async fn refinement(&mut self, task: &str, last_code_attempt: &str, feedback: &str) -> Option<String> {
-        let prompt = format!(r#"
-            你是一位资深的Python程序员。你正在根据一位代码评审专家的反馈来优化你的代码。
-
-            # 原始任务:
-            {task}
-
-            # 你上一轮尝试的代码:
-            {last_code_attempt}
-            评审员的反馈：
-            {feedback}
-
-            请根据评审员的反馈，生成一个优化后的新版本代码。
-            你的代码必须包含完整的函数签名、文档字符串，并遵循PEP 8编码规范。
-            请直接输出优化后的代码，不要包含任何额外的解释。
-
-            请分析该代码的时间复杂度，并思考是否存在一种<strong>算法上更优</strong>的解决方案来显著提升性能。
-            如果存在，请清晰地指出当前算法的不足，并提出具体的、可行的改进算法建议（例如，使用筛法替代试除法）。
-            如果代码在算法层面已经达到最优，才能回答“无需改进”。
-
-            请直接输出你的反馈，不要包含任何额外的解释。
-        "#);
-
-        self.messages.push(
-            ChatCompletionMessage {
-                role: MessageRole::user,
-                content: Content::Text(prompt),
-                name: None,
-                tool_call_id: None,
-                tool_calls: None
-            }
-        );
-        let think_stream = self.llm_client.think(self.messages.clone(), None, None).await;
-        let (refine, is_no_call_tool) = self.process_think_stream(Box::pin(think_stream), None).await;
-        if is_no_call_tool {
-            Some(refine)
-        } else {
-            None
-        }
-    }
-
-    async fn process_think_stream(&mut self, mut think_stream: Pin<Box<impl Stream<Item = ChatCompletionStreamResponse>>>, is_remmenber: Option<bool>) -> (String, bool) {
-        let is_remmenber = is_remmenber.unwrap_or(true);
-        let (mut is_first_print_content, mut is_first_print_reason) = (true, true);
-
-        let (mut reason_delta, mut content_delta) = (vec![], vec![]);
-        let mut tools_call = None; 
-
-        while let Some(chunck) = think_stream.next().await {
-            match chunck {
-                ChatCompletionStreamResponse::Content(delta) => {
-                    if is_first_print_content {
-                        println!("\n\nAI: ");
-                        is_first_print_content = false;
-                    }
-                    print!("{}", delta);
-                    content_delta.push(delta);
-
-                },
-                ChatCompletionStreamResponse::Reasoning(delta) => {
-                    if is_first_print_reason {
-                        // println!("\n\nTHINK: ");
-                        is_first_print_reason = false;
-                    }
-                    // print!("{}", delta);
-                    reason_delta.push(delta);
-                },
-                ChatCompletionStreamResponse::ToolCall(tc) => {
-                    tools_call = Some(tc);
-                },
-                ChatCompletionStreamResponse::Done(_finish_reason)=> {
-                    // 区分调用工具和没有调用工具的信息
-
-                    // message 处理，工具调用处理（工具本身调用也可以作为一个流，但是本次就先做简单版本）
-                    if is_remmenber {
-                        self.messages.push(
-                            ChatCompletionMessage { role: MessageRole::assistant, content: Content::Text(reason_delta.join("")), name: None, tool_calls: None, tool_call_id: None },
-                        );
-                    }
-                    // tool call
-                    if let Some(tools_call) = &tools_call {
-                        
-                        let tasks = tools_call
-                            .iter()
-                            .map(|tool_call| self.tool_manager.run(tool_call.clone()))
-                            .collect::<Vec<_>>();
-                        
-                        let tools_call_result = futures_util::future::join_all(tasks).await;
-                        // 工具调用
-                        if is_remmenber {
-                            self.messages.push(
-                                ChatCompletionMessage { role: MessageRole::assistant, content: Content::Text(content_delta.join("")), tool_calls: Some(tools_call.clone()), name: None, tool_call_id: None }
-                            );
+                        match finish_reason {
+                            FinishReason::tool_calls | FinishReason::function_call => {
+                                let tools_call = tools_call.clone().unwrap_or_default().clone();
+                                self.add_message(Message::assistant(reason_delta.join(""), None));
+                                self.add_message(Message::assistant_with_tools(content_delta.join(""), tools_call.clone(),None));
+                                // 工具调用开始
+                                let tasks = tools_call
+                                    .iter()
+                                    .map(|tool_call| self.tool_manager.run(tool_call.clone()))
+                                    .collect::<Vec<_>>();
+                                
+                                let tools_call_result = futures_util::future::join_all(tasks).await;
+                                
+                                tools_call_result
+                                    .into_iter()
+                                    .for_each(|(tool_call_id, tool_call_result)| {
+                                        let tool_call_result = match tool_call_result {
+                                            Ok(content) => content,
+                                            Err(error_msg) => error_msg,
+                                        };
+                                        println!("tool_call_result: {}", tool_call_result);
+                                        self.add_message(Message::tool_response(tool_call_id, tool_call_result, None));
+                                    });
+                            },
+                            _ => {
+                                self.add_message(Message::assistant(reason_delta.join(""), None));
+                                self.add_message(Message::assistant(content_delta.join(""), None));
+                                final_resut = content_delta.join("");
+                                is_continue = false;
+                                break;
+                            },
                         }
-                        // 工具调用结果
-                        tools_call_result
-                            .into_iter()
-                            .for_each(|(tool_call_id, tool_call_result)| {
-                                let tool_call_result = match tool_call_result {
-                                    Ok(content) => content,
-                                    Err(error_msg) => error_msg,
-                                };
-                                println!("tool_call_result: {}", tool_call_result);
-                                if is_remmenber {
-                                    self.messages.push(
-                                        ChatCompletionMessage { role: MessageRole::tool, content: Content::Text(tool_call_result), tool_call_id: Some(tool_call_id), name: None, tool_calls: None }
-                                    )
-                                }
-                            });     
-                    } else {
-                        if is_remmenber {
-                            self.messages.push(
-                                ChatCompletionMessage { role: MessageRole::assistant, content: Content::Text(content_delta.join("")), name: None, tool_calls: None, tool_call_id: None }
-                            );
-                        }
-                    }
-                },
-            }
 
-            std::io::Write::flush(&mut std::io::stdout());
-            
+                        current_step += 1;
+                        if current_step == self.max_steps {
+                            is_continue = false;
+                            println!("抱歉，我无法在限定步数内完成这个任务。");
+                            let _ = std::io::Write::flush(&mut std::io::stdout());
+                        }
+                    },
+                }
+                let _ = std::io::Write::flush(&mut std::io::stdout());
+            }
+            println!();
         }
 
-        (content_delta.join(""), tools_call.is_none())
+        final_resut
+    }
+
+    fn add_message(&mut self, message: crate::base::message::Message) {
+        self.base_mut().add_message(message);
+    }
+    
+    fn clear_history(&mut self) {
+        self.base_mut().clear_history();
+    }
+    
+    fn get_history(&self) -> Vec<crate::base::message::Message> {
+        self.base().get_history()
+    }
+    
+    fn description(&self) -> String {
+        format!(
+            "Agent(name={}, provider={})",
+            self.base().name,
+            self.base().llm.provider
+        )
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use agent_lab::{agent::{react_agent::ReActAgent, simple_agent::SimpleAgent}, base::{agent::Agent, config::Config, llm::AgentsLLM}, tools::{ToolManager, web_search::WebSearch}};
+use agent_lab::{agent::{react_agent::ReActAgent, reflection_agent::{ReflectionAgent, ReflectionPromptTemplates}, simple_agent::SimpleAgent}, base::{agent::Agent, config::Config, llm::AgentsLLM}, tools::{ToolManager, web_search::WebSearch}};
 
 #[tokio::main]
 async fn main() -> () {
@@ -12,14 +12,28 @@ async fn main() -> () {
     //     true,
     // );
 
-    let mut react_agent = ReActAgent::new(
+    // let mut react_agent = ReActAgent::new(
+    //     "基础助手",
+    //     AgentsLLM::from_env(),
+    //     "你是一个友好的MasterGo AI助手，请用简洁明了的方式回答问题。".to_string(),
+    //     Config::from_env(),
+    //     ToolManager::new()
+    //         .with_tool(Box::new(WebSearch::new())),
+    //     5,
+    // );
+
+    let mut reflection_agent = ReflectionAgent::new(
         "基础助手",
         AgentsLLM::from_env(),
-        "你是一个友好的MasterGo AI助手，请用简洁明了的方式回答问题。".to_string(),
+        ReflectionPromptTemplates {
+            initial: "你是Python专家，请编写函数:{task}".into(),
+            reflect: "请审查代码的算法效率:\n任务:{task}\n代码:{content}".into(),
+            refine: "请根据反馈优化代码:\n任务:{task}\n反馈:{content}".into(),
+        },
         Config::from_env(),
         ToolManager::new()
             .with_tool(Box::new(WebSearch::new())),
         5,
     );
-    let _ = react_agent.run("帮我找找肖元彪都有哪些人").await;
+    let _ = reflection_agent.run_reflection("帮我找到1000以内的素数").await;
 }
