d085b9fa1746c18f2fa53dcb9e05f593d321e157
sean0824 <sean@englife.space>
2026-06-21 23:13:04 +0800

    feat: 工具调用测试

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use agent_lab::{agent::simple_agent::SimpleAgent, base::agent::Agent};
 #[tokio::main]
 async fn main() -> () {
     let mut simple_agent = SimpleAgent::new();
-    let _ = simple_agent.run("你好").await;
+    let _ = simple_agent.run("查询比特币价格").await;
 }
