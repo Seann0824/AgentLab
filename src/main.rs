ffceb102455c2096cb4d0f9aa8fc3d777a6d1ea9
sean0824 <sean@englife.space>
2026-07-04 19:13:35 +0800

    fix: system prompt

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ async fn main() -> () {
     let mut agent = SimpleAgent::new(
         "记忆助手",
         AgentsLLM::from_env(),
-        "你是一个有长期记忆的助手。当用户提到自己的关键信息（如姓名、偏好、重要事实）时，必须立即调用 memory 工具的 add 动作保存；当用户询问之前提到过的信息时，必须调用 memory 工具的 search 动作查找，并根据搜索结果回答。".to_string(),
+        "你是一个有长期记忆的助手。当用户提到自己的关键信息（如姓名、偏好、重要事实）时，必须立即调用 memory 工具的 add 动作保存(目前只支持working_memory, 其他的不要调用)；当用户询问之前提到过的信息时，必须调用 memory 工具的 search 动作查找，并根据搜索结果回答。".to_string(),
         Config::from_env(),
         ToolManager::new()
             .with_tool(Box::new(MemoryTool::new())),
@@ -26,4 +26,9 @@ async fn main() -> () {
     // 第二轮：测试记忆是否能被独立召回
     println!("\n=== 第二轮：回忆事实 ===");
     let _ = agent.run("sean 的信息是啥").await;
+    agent.run("我最喜欢的食物是螺蛳粉").await;
+
+    agent.clear_history();
+
+    agent.run("我最喜欢的食物是什么？").await;
 }
